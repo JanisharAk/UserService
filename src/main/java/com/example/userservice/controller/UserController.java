ffc9a2fa0c1bcf6f71a6ae15d5b18527da3e1f8d
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-07-16 14:59:35 +0530

    controller,dtos,pom,application.prop

---
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.userservice.controller;
+
+
+import com.example.userservice.models.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    public User login() {
+        // check if email and password in db
+        // if yes return user
+        // else throw some error
+        return null;
+    }
+
+    public User signUp() {
+        // no need to hash password for now
+        // just store user as is in the db
+        // for now no need to have email verification either
+        return null;
+    }
+
+    public ResponseEntity<Void> logout() {
+        // delete token if exists -> 200
+        // if doesn't exist give a 404
+        return null;
+    }
+}
