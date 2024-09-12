227d311fe088567eda8a4296d604ff4d04df7534
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-09-12 23:39:18 +0530

    auth part4

--- src/main/java/com/example/userservice/security/SecurityConfig.java
+++ src/main/java/com/example/userservice/security/SecurityConfig.java
@@ -67,10 +67,9 @@ public class SecurityConfig {
             throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
-                .csrf().disable()
-                .cors().disable()
+
                 // Form login handles the redirect to the login page from the
                 // authorization server filter chain
                 .formLogin(Customizer.withDefaults());
