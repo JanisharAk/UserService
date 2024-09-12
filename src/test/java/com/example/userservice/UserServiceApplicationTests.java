04ce52ba7ee45c43d3eb2fc4be03bb6bebf53799
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-09-12 22:48:27 +0530

    auth part3

--- src/test/java/com/example/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/example/userservice/UserServiceApplicationTests.java
@@ -1,10 +1,16 @@
 package com.example.userservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 
 @SpringBootTest
 class UserServiceApplicationTests {
+
+    @Autowired
+    private RegisteredClientRepository registeredClientRepository;
+
 
     @Test
     void contextLoads() {
